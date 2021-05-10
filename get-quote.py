01de84dc7a5f5dde017d5a1bc948b5ead9aa85ae
Juan Bruce <j.sebastianbruce@icloud.com>
2021-05-09 22:07:25 -0500

    File writing

--- get-quote.py
+++ get-quote.py
@@ -2,14 +2,18 @@ import random
 def main():
   #print("Keep it logically awesome.")
 
+  a = open("quotes.txt", "a")
+  a.write("Now the file has more content!")
+  a.close()
+
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  last_1 = 19
+  last_1 = 20
   rnd_1 = random.randint(0, last_1)
 
-  last_2 = 19
+  last_2 = 20
   rnd_2 = random.randint(0, last_2)
 
   print(quotes[rnd_1].strip('\n'))
